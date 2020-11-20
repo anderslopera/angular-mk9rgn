fc2a4e255e27827e406c3e42bffbb02246916f78
anderslopera <anderslopera@gmail.com>
2020-11-20 14:58:51 -0500

    Lightbox Complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,37 +1,38 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
-import { ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { RouterModule } from "@angular/router";
+import { ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
 
-import { AppComponent } from './app.component';
-import { TopBarComponent } from './top-bar/top-bar.component';
-import { ProductListComponent } from './product-list/product-list.component';
-import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
-import { ProductDetailsComponent } from './product-details/product-details.component';
-import { MyResumeComponent } from './my-resume/my-resume.component';
-import { ResumeEducationComponent } from './resume-education/resume-education.component';
-import { ResumeFooterComponent } from './resume-footer/resume-footer.component';
-import { ResumeProfileComponent } from './resume-profile/resume-profile.component';
-import { ResumeWorkExperienceComponent } from './resume-work-experience/resume-work-experience.component';
-import { CartService } from './cart.service';
-import { CartComponent } from './cart/cart.component';
-import { ShippingComponent } from './shipping/shipping.component';
-import { GraphicDesignComponent } from './graphic-design/graphic-design.component';
+import { AppComponent } from "./app.component";
+import { TopBarComponent } from "./top-bar/top-bar.component";
+import { ProductListComponent } from "./product-list/product-list.component";
+import { ProductAlertsComponent } from "./product-alerts/product-alerts.component";
+import { ProductDetailsComponent } from "./product-details/product-details.component";
+import { MyResumeComponent } from "./my-resume/my-resume.component";
+import { ResumeEducationComponent } from "./resume-education/resume-education.component";
+import { ResumeFooterComponent } from "./resume-footer/resume-footer.component";
+import { ResumeProfileComponent } from "./resume-profile/resume-profile.component";
+import { ResumeWorkExperienceComponent } from "./resume-work-experience/resume-work-experience.component";
+import { CartService } from "./cart.service";
+import { CartComponent } from "./cart/cart.component";
+import { ShippingComponent } from "./shipping/shipping.component";
+import { GraphicDesignComponent } from "./graphic-design/graphic-design.component";
+import { LightboxModule } from "ngx-lightbox";
 
 @NgModule({
   imports: [
     BrowserModule,
     HttpClientModule,
     ReactiveFormsModule,
+    LightboxModule,
     RouterModule.forRoot([
-      { path: '', component: ProductListComponent },
-      { path: 'products/:productId', component: ProductDetailsComponent },
-      { path: 'resume', component: MyResumeComponent },
-      { path: 'cart', component: CartComponent },
-      { path: 'shipping', component: ShippingComponent },
-      { path: 'graphic', component: GraphicDesignComponent },
-
+      { path: "", component: ProductListComponent },
+      { path: "products/:productId", component: ProductDetailsComponent },
+      { path: "resume", component: MyResumeComponent },
+      { path: "cart", component: CartComponent },
+      { path: "shipping", component: ShippingComponent },
+      { path: "graphic", component: GraphicDesignComponent }
     ])
   ],
   declarations: [
@@ -49,11 +50,10 @@ import { GraphicDesignComponent } from './graphic-design/graphic-design.componen
     ShippingComponent,
     GraphicDesignComponent
   ],
-  bootstrap: [ AppComponent ],
+  bootstrap: [AppComponent],
   providers: [CartService]
 })
-export class AppModule { }
-
+export class AppModule {}
 
 /*
 Copyright Google LLC. All Rights Reserved.
--- src/app/graphic-design/graphic-design.component.ts
+++ src/app/graphic-design/graphic-design.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 
+import { Lightbox } from "ngx-lightbox";
+
 import { CartService } from "../cart.service";
 
 @Component({
@@ -9,10 +11,27 @@ import { CartService } from "../cart.service";
 })
 export class GraphicDesignComponent implements OnInit {
   portfolioItems;
+  _albums: any = [];
+  impHeight = 357;
 
-  constructor(private cartService: CartService) {}
+  constructor(private cartService: CartService, private _lightbox: Lightbox) {}
 
   ngOnInit() {
     this.portfolioItems = this.cartService.getGraphicDesignPortfolio();
+    this.cartService.getGraphicDesignPortfolio().subscribe(data => {
+      console.log(data);
+      this._albums = data;
+    });
+  }
+
+  open(index: number): void {
+    //open Lightbox
+
+    this._lightbox.open(this._albums, index);
+  }
+
+  close(): void {
+    //close Lightbox
+    this._lightbox.close();
   }
 }
