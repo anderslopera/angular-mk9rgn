19443c7ad78fa0511c8d7c4dc11b783c514e4c0b
anderslopera <anderslopera@gmail.com>
2020-11-20 13:52:58 -0500

    graphic display error

--- src/app/graphic-design/graphic-design.component.ts
+++ src/app/graphic-design/graphic-design.component.ts
@@ -1,22 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
-import { CartService } from '../cart.service';
+import { CartService } from "../cart.service";
 
 @Component({
-  selector: 'app-graphic-design',
-  templateUrl: './graphic-design.component.html',
-  styleUrls: ['./graphic-design.component.css']
+  selector: "app-graphic-design",
+  templateUrl: "./graphic-design.component.html",
+  styleUrls: ["./graphic-design.component.css"]
 })
 export class GraphicDesignComponent implements OnInit {
-
   portfolioItems;
 
-  constructor(
-    private cartService: CartService
-  ) { }
+  constructor(private cartService: CartService) {}
 
   ngOnInit() {
     this.portfolioItems = this.cartService.getGraphicDesignPortfolio();
   }
-
 }
